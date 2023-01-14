8a44189068a01e5e8b146f534033947bdaa0ea62
eskimo <info@eskimo.sh>
2023-01-15 00:22:29 +0100

    test fix

--- src/test/javawebtests/ch/niceideas/eskimo/html/EskimoNotificationsTest.java
+++ src/test/javawebtests/ch/niceideas/eskimo/html/EskimoNotificationsTest.java
@@ -56,6 +56,8 @@ public class EskimoNotificationsTest extends AbstractWebTest {
         // instantiate test object
         js("eskimoNotifications = new eskimo.Notifications();");
         js("eskimoNotifications.initialize()");
+
+        waitForElementIdInDOM("new-notifications-count");
     }
 
     @Test
